ffd693ab927bcd4772512c1aa685a12ef831b5a6
masaki <masaki@192.168.3.83>
2025-02-16 18:16:43 +0900

    ハンドラーメソッドmodel使用パターン作成

--- src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
+++ src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 /**
  * 
@@ -16,7 +17,19 @@ public class FocusTimeApplicationcontroller {
 	@GetMapping("/FocusTime")
 	public String FocusTime() {
 		return  "test";
+		
 	}
+	
+	//modelのやり方
+		@GetMapping("/FocusTime")
+		//public Stringは変えられない
+		
+		public String FocusTimemodel(Model model) {
+			//bodyweight=体重
+			model.addAttribute("bodyweight", "7999");
+			model.addAttribute("welcome", "Welcome MVC (Model)");
+			return "test";
+		}
 
 }
 
