fa452019862162223671a6276936514ec49132ce
masaki <masaki@192.168.3.83>
2025-02-16 18:17:24 +0900

    ハンドラーメソッド、ModelAndViewの場合作成

--- src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
+++ src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 /**
  * 
  * @author masaki
@@ -14,21 +15,34 @@ public class FocusTimeApplicationcontroller {
 	 * 
 	 * @return test.html
 	 */
-	@GetMapping("/FocusTime")
-	public String FocusTime() {
-		return  "test";
-		
-	}
 	
-	//modelのやり方
+	
+	    //modelのやり方
 		@GetMapping("/FocusTime")
 		//public Stringは変えられない
 		
 		public String FocusTimemodel(Model model) {
 			//bodyweight=体重
 			model.addAttribute("bodyweight", "7999");
-			model.addAttribute("welcome", "Welcome MVC (Model)");
+			//Content=科目
+			model.addAttribute("Content", "科目");
 			return "test";
+		}
+		
+		
+		
+		
+		//ModelAndViewのやり方
+		@GetMapping("/FocusTime-mv")
+		
+		public ModelAndView FocusTimeeMv(ModelAndView mv) {
+			//bodyweight=体重
+			mv.addObject("bodyweight", "7999");
+			//Content=科目
+			mv.addObject("Content", "科目");
+			mv.setViewName("test");
+			
+			return mv;
 		}
 
 }
