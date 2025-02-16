6525f0c8f02d4638fcc2f91ead7a569219ce8e62
masaki <masaki@192.168.3.83>
2025-02-16 14:16:35 +0900

    FocusTimeApplicationへ変更

--- src/test/java/com/example/demo/FocusTimeApplicationTests.java
+++ src/test/java/com/example/demo/FocusTimeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class motoApplicationTests {
+class FocusTimeApplicationTests {
 
 	@Test
 	void contextLoads() {
