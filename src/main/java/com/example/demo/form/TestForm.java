864e0769dfca417d7f369bd6d23d2f9b4c0c482b
masaki <masaki@192.168.3.82>
2025-02-20 22:25:04 +0900

    validation終了

--- src/main/java/com/example/demo/form/TestForm.java
+++ src/main/java/com/example/demo/form/TestForm.java
@@ -3,6 +3,8 @@ package com.example.demo.form;
 import java.sql.Date;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
 //lombokのデータアノテーションでgetter、setter省略
@@ -17,12 +19,13 @@ public class TestForm {
 		private Date study;
 		//勉強時間
 		@Min(value=1,  message="入力してください。")
+		@NotNull(message = "入力してください。")
 		private Integer studyhours;
 		//教科
-		
+		@NotBlank(message = "内容を入力してください。")
 		private String subject;
 		//内容
-		
+		@NotBlank(message = "内容を入力してください。")
 		private String comment;
 }
 
