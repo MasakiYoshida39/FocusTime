74fcd7ad237a66cf3036559613a657d1a12721cb
masaki <masaki@192.168.3.82>
2025-02-20 21:05:42 +0900

    validation追加

--- src/main/java/com/example/demo/form/TestForm.java
+++ src/main/java/com/example/demo/form/TestForm.java
@@ -2,6 +2,9 @@ package com.example.demo.form;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 //lombokのデータアノテーションでgetter、setter省略
 @Data
@@ -11,12 +14,17 @@ public class TestForm {
 		private String userId;
 	*/
 		//勉強日
+	    @Past(message="今日以前の日付を入力してください。")
 		private Date study;
 		//勉強時間
+		@NotNull(message="入力してください。")
+		@Min(value=1,  message="正の整数を入力してください。")
 		private Integer studyhours;
 		//教科
+		@NotNull(message="入力してください。")
 		private String subject;
 		//内容
+		@NotNull(message="入力してください。")
 		private String comment;
 }
 
