015a6754cfdca28f88c44772a83f15815fb25aff
masaki <masaki@192.168.3.82>
2025-02-24 16:06:53 +0900

    テスト用インターフェイス実装クラス作成

---
+++ src/main/java/com/example/demo/mock/RegistServiceMock.java
@@ -0,0 +1,13 @@
+package com.example.demo.mock;
+
+import com.example.demo.service.RegistService;
+
+public class RegistServiceMock implements RegistService {
+
+	@Override
+	public String regist() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "テスト的に動きました。";
+	}
+
+}
