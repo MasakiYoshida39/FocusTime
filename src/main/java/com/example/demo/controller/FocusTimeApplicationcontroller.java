46efb6f96cd9a9c7f0b42fe38593f3796b5e8a63
masaki <masaki@192.168.3.82>
2025-02-24 16:10:36 +0900

    不要なコメント削除

--- src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
+++ src/main/java/com/example/demo/controller/FocusTimeApplicationcontroller.java
@@ -64,51 +64,10 @@ public class FocusTimeApplicationcontroller {
 			
 			return "complete";
 		}
-		/**
-		 
-		// /post-paramのポストからリクエストを受け取る
-		@PostMapping("/post-param")
-		//content=科目
-		public String postParam(@RequestParam String content,
-				Model model) {
-			model.addAttribute("mGreeting", content);
-			
-			return "test";
-		}
 	
-		
-	    //modelのやり方
-		@GetMapping("/FocusTime")
-		//public Stringは変えられない
-		
-		public String FocusTimemodel(Model model) {
-			//bodyweight=体重
-			model.addAttribute("bodyweight", "799");
-			//Content=科目
-			model.addAttribute("Content", "理科");
-			return "test";
-		}
-	
-		
-		
-		
-		//ModelAndViewのやり方
-		@GetMapping("/FocusTime-mv")
-		
-		public ModelAndView FocusTimeeMv(ModelAndView mv) {
-			//bodyweight=体重
-			mv.addObject("bodyweight", "7999");
-			//Content=科目
-			mv.addObject("Content", "科目");
-			mv.setViewName("test");
-			
-			return mv;
-		}
-		*/
 
 }
 
 
 
 
-// /**Enter = JavaDocテンプレート作成
