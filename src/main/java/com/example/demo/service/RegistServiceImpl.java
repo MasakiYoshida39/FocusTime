2660f2c0494629e77253e82f4a8241c772524e5f
masaki <masaki@192.168.3.82>
2025-02-24 16:02:00 +0900

    インターフェイス実装クラス作成

---
+++ src/main/java/com/example/demo/service/RegistServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+public class RegistServiceImpl implements RegistService {
+
+	@Override
+	public String regist() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
