360f747f6a6010eb6faea13b06ddcf214a1006d4
masaki <masaki@192.168.3.82>
2025-02-24 18:37:20 +0900

    RegistServiceImpl作成

--- src/main/java/com/example/demo/service/RegistServiceImpl.java
+++ src/main/java/com/example/demo/service/RegistServiceImpl.java
@@ -2,12 +2,21 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Learning;
+import com.example.demo.repository.RegistRepository;
+
+import lombok.RequiredArgsConstructor;
 @Service
+@RequiredArgsConstructor
 public class RegistServiceImpl implements RegistService {
+	
+	private final RegistRepository repository;
 
 	@Override
-	public String regist() {
+	public void regist(Learning learning) {
 		// TODO 自動生成されたメソッド・スタブ
-		return "レビュー登録が完了しました。";	}
+		repository.add(learning);
+
+	}
 
 }
